2102afa61ba4ff57eae6a71af86638fcf5a5cc6b
BOUGARRANI <93446329+BOUGARRANi@users.noreply.github.com>
2022-12-29 14:56:15 +0100

    Push dashboard

---
+++ src/Components/Header/NavbarComponent.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  AppBar,
+  Box,
+  Hidden,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { useStyles } from "./HeaderStyle";
+import MenuRoundedIcon from "@material-ui/icons/MenuRounded";
+
+import Notification from "./ActionTab/Notification";
+import Profile from "./ActionTab/Profile";
+import Messages from "./ActionTab/Messages";
+
+export default function NavbarComponent({ handleDrawerToggle }) {
+  const classes = useStyles();
+
+  return (
+    <AppBar>
+      <Toolbar className={classes.toolbar}>
+        <Box style={{ display: "flex" }}>
+          <Typography variant="h5" className={classes.logo}>
+            {"<AdminPannel />"}
+          </Typography>
+        </Box>
+        <Hidden smDown>
+          <Box>
+            <Profile />
+          </Box>
+        </Hidden>
+        <Hidden mdUp>
+          <IconButton color="inherit" onClick={handleDrawerToggle}>
+            <MenuRoundedIcon />
+          </IconButton>
+        </Hidden>
+      </Toolbar>
+    </AppBar>
+  );
+}
